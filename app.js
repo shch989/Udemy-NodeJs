faacc595b8c9c08da573c524856aee8c3301cfcd
shch989 <tankpop11@gmail.com>
2022-11-20 20:33:00 +0900

    2022-11-20

--- app.js
+++ app.js
@@ -4,21 +4,27 @@ const path = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
 // Router require
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
+// bodyParser 기본설정
 app.use(bodyParser.urlencoded({extended: false}));
+// css 경로 추적
 app.use(express.static(path.join(__dirname, 'public')))
 
 const port = 5000
 
 // Router
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
+// 404 페이지
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', {pageTitle : 'Page Not Found'})
 })
 
 app.listen(port, () => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,22 @@
-const path = require('path')
+const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../helper/path')
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-})
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
